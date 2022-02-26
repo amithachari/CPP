568f1fdf4f2d65adec525f07cd571853d8a0b964
amithachari <64373075+amithachari@users.noreply.github.com>
2022-02-26 11:09:45 -0600

    Create goal.cpp

---
+++ goal.cpp
@@ -0,0 +1,37 @@
+/*Goal: In the programming quiz, use a while loop to prompt
+**the user to guess a target number. 
+**Tell the user if the guess is too high or too low. 
+**The user enters -1  or guesses the target number to end 
+**the program.
+*/
+
+#include <iostream>
+#include<sstream>
+
+int main()
+{
+    //use 55 as the number to be guessed
+    int target = 55;
+    int guess = -1;
+    
+    while(guess != target){
+        std::cout<<"Guess a number between 0 and 100: ";
+        std::cin>>guess;
+        std::cout<<guess<<"\n";
+        if (guess < target){
+            std::cout<<"guess is too low";
+        }
+        else if(guess > target){
+            std::cout<<"guess is too high";
+        }
+        else{
+            std::cout<<"You guessed the target!\n";
+        }
+        if(guess == -1){
+            std::cout<<"good bye!";
+            break;
+        }
+    }
+            
+    return 0;
+}
